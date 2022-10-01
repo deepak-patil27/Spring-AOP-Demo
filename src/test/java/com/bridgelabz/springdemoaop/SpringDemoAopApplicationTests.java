d6ee22447fe1a47365b5c2913f538ec712787c92
Awaskar <akash.awaskar@aciworldwide.com>
2022-10-01 14:28:54 +0530

    Created Learning Project Of Spring AOP

---
+++ src/test/java/com/bridgelabz/springdemoaop/SpringDemoAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springdemoaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
